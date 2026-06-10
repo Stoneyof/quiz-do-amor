f38fd7ae8daad0c6463c4e219b887e39aa289d8e
Stoney <91440800+Stoneyof@users.noreply.github.com>
2026-06-10 00:40:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function abrirCarta() {
 
 // --- CONTROLE DA CÂMERA EM TEMPO REAL ---
 
-function activarCameraEmTempoReal() {
+function ativarCameraEmTempoReal() {
     btnIniciarCamera.classList.add('escondido');
     cameraInterface.classList.remove('escondido');
 
